2a2c6b873272cfce0be9ee5f2ded45f2ffcf7cff
MatthewTan420 <s10239950@connect.np.edu.sg>
2023-12-23 22:31:22 +0800

    auth_and_database

--- firebase.js
+++ firebase.js
@@ -21,19 +21,26 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 const db = getDatabase();
+const playerRef = ref(db, "players");
+
+class userID { 
+    static userID = "";
+}
 
 import {
     getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut
     } from 'https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js';
     const auth = getAuth();
     var frmCreateUser = document.getElementById("frmCreateUser");
-    frmCreateUser.addEventListener("submit", function(e){
+    if (frmCreateUser != null){
+        frmCreateUser.addEventListener("submit", function(e){
         e.preventDefault();
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
         createUser(email, password);
         console.log("email" + email + "password" + password);
     });
+    }
     function createUser(email, password){
         console.log("creating the user");
         createUserWithEmailAndPassword(auth, email, password)
@@ -41,32 +48,139 @@ import {
         const user = userCredential.user;
         console.log("created user ... " + JSON.stringify(userCredential));
         console.log("User is now signed in ");
+        window.location.assign('createUser.html');
     }).catch((error)=>{
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(`ErrorCode: ${errorCode} -> Message: ${errorMessage}`);
     });
+
+    onAuthStateChanged(auth, (currentUser) => {
+        if (currentUser) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+            const uid = currentUser.uid;
+            console.log(`(OnAuthStateChanged) Current user is logged in:
+        ${currentUser.email} :: ${currentUser.uid}`)
+            console.log(userID.userID);
+            userID.userID = currentUser.uid;
+            console.log(userID.userID);
+        } else {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(`ErrorCode: ${errorCode} -> Message: ${errorMessage}`);
+        }
+    });
 }
 
 import {
-getDatabase, ref, child, get,set,onValue,push,update,remove
-} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+    getDatabase, ref, child, get,set,onValue,push,update,remove
+    } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
 
-var addUserToDatabase = document.getElementById("frmCreateData");
-addUserToDatabase.addEventListener("submit", function(e){
-    e.preventDefault();
-    var username = document.getElementById("username").value;
-    var country = document.getElementById("country").value;
-    var age = document.getElementById("age").value;
-    createData(username, country, age,)
-})
+    var addUserToDatabase = document.getElementById("frmCreateData");
+    if (addUserToDatabase != null){
+        addUserToDatabase.addEventListener("submit", function(e){
+        e.preventDefault();
+        var username = document.getElementById("username").value;
+        var country = document.getElementById("country").value;
+        var age = document.getElementById("age").value;
+        createData(username, country, age, 0, 0, false)
+    })
+    }
+    function createData(username, country, age, time, points, admin){
+        const userRef = ref(db, `players/${username}`);
+        //const newUserRef = push(userRef);
+        set(userRef, {
+            username: username,
+            country: country,
+            age: age,
+            time: time,
+            points: points,
+            admin: admin,
+        })
 
-function createData(username, country, age){
-    const userRef = ref(db, `players/${username}`);
-    const newUserRef = push(userRef);
-    set(newUserRef, {
-        username: username,
-        country: country,
-        age: age,
-    });
-}
+        .then(function(){
+            console.log("User has created data ");
+            window.location.href='displayData.html';
+        });
+        
+    }
+
+    var frmlogUser = document.getElementById("frmlogUser");
+            if (frmlogUser != null){
+                frmlogUser.addEventListener("submit", function(e){
+                e.preventDefault();
+                var email = document.getElementById("email").value;
+                var password = document.getElementById("password").value;
+                loginUser(email, password);
+                console.log("email" + email + "password" + password);
+            });
+            }
+
+            function loginUser(email, password){
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log("login");
+                window.location.href='displayData.html';
+                // ...
+            })
+                .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(`ErrorCode: ${errorCode} -> Message: ${errorMessage}`);
+            });
+        }
+
+        var readBtn = document
+            .getElementById("btn-read")
+            .addEventListener("click", getPlayerData);
+
+    function getPlayerData(){
+        get(playerRef).then((snapshot) => {
+            if(snapshot.exists()){
+                console.log(snapshot.val());
+                let table = document.getElementById("player-data");
+                let playerInfo = "";
+                snapshot.forEach((childSnapshot) => {
+                    playerInfo += `<tr>
+                        <td>${childSnapshot.val().username}</td>
+                        <td>${childSnapshot.val().country}</td>
+                        <td>${childSnapshot.val().age}</td>
+                        </tr>`;
+                });
+                table.innerHTML = playerInfo
+            } else{
+                console.log("No data");
+            }
+    }).catch((error) => {
+        console.error(error);
+    })
+    }
+
+    var adminBtn = document
+            .getElementById("btn-admin")
+            .addEventListener("click", getAdminData);
+    
+    function getAdminData(){
+        get(playerRef).then((snapshot) => {
+            if(snapshot.exists()){
+                console.log(snapshot.val());
+                snapshot.forEach((childSnapshot) => {
+                    if (childSnapshot.val().admin == true) {
+                        if (childSnapshot.key == userID.userID) {
+                            console.log("access granted");
+                            window.location.href='adminPage.html';
+                        }
+                    } else {
+                        console.log("not admin");
+                    };
+                });
+            } else{
+                console.log("No data");
+            }
+        }).catch((error) => {
+            console.error(error);
+        })
+        }
